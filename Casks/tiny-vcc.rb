bd30f767249b49996a186af5731e9a9d5a9ea575
babo <babomancer@gmail.com>
2023-11-08 19:16:25 -0800

    tiny-vcc 0.6.0 (new cask)

---
+++ Casks/tiny-vcc.rb
@@ -0,0 +1,30 @@
+cask "tiny-vcc" do
+  version "0.6.0"
+  sha256 "d6e45f7e26ed9646aef510153a26bdd79cdf0c88020d82420e1f7aebb8ad1772"
+
+  url "https://github.com/kurotu/tiny_vcc/releases/download/v#{version}/TinyVCC-v#{version}.dmg"
+  name "Tiny VCC"
+  desc "Alternative client for VRChat Creator Companion"
+  homepage "https://github.com/kurotu/tiny_vcc"
+
+  app "Tiny VCC.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.github.kurotu.tiny-vcc",
+    "~/Library/Caches/tiny_vcc",
+    "~/Library/Saved Application State/com.github.kurotu.tiny-vcc.savedState",
+  ]
+
+  caveats <<~EOS
+    #{token} requires .NET 6, get it from:
+      https://dotnet.microsoft.com/en-us/download/dotnet/6.0
+    or
+      brew install isen-ng/dotnet-sdk-versions/dotnet-sdk6-0-400
+
+    #{token} requires Unity Hub and an appropriate version of Unity:
+      brew install unity-hub
+
+    #{token} requires VPM CLI:
+      dotnet tool install --global vrchat.vpm.cli
+  EOS
+end
