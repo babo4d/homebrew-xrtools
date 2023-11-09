00ed98c052d3f09fc6222a1743cdc3f13bff54a0
babo <babomancer@gmail.com>
2023-11-08 17:52:09 -0800

    vrc-get 1.3.1 (new formula)
    from: <https://github.com/anatawa12/homebrew-core>

---
+++ Formula/vrc-get.rb
@@ -0,0 +1,24 @@
+class VrcGet < Formula
+  desc "Command-line VPM Client"
+  homepage "https://github.com/anatawa12/vrc-get"
+  url "https://github.com/anatawa12/vrc-get/archive/v1.3.1.tar.gz"
+  sha256 "3d91db43d528f52defc223f1f20bba9d680108166d562eacc564f8a3d686fa4f"
+  license "MIT"
+
+  head "https://github.com/anatawa12/vrc-get.git", branch: "master"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--root", prefix, "--path", "."
+  end
+
+  test do
+    assert_match "vrc-get", shell_output("#{bin}/vrc-get --help")
+  end
+end
