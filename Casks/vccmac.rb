1706aed00a620c99eb2c8496af7d1ec22f033c92
babo <babomancer@gmail.com>
2023-11-08 19:15:38 -0800

    vccmac 1.0.4 (new cask)

---
+++ Casks/vccmac.rb
@@ -0,0 +1,18 @@
+cask "vccmac" do
+  version "1.0.4"
+  sha256 "5cb94383da0248d5d97d9e3f3d5e2cb0d26aaa7e28f74c93598113d2fcb6024e"
+
+  url "https://github.com/AranoYuki1/VCC-for-mac/releases/download/#{version}/VCCMac.app.zip"
+  name "VCC for mac"
+  desc "Unofficial client for VRChat Creator Companion"
+  homepage "https://github.com/AranoYuki1/VCC-for-mac"
+
+  app "VCCMac.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.yuki.VCCMac",
+    "~/Library/Logs/com.yuki.VCCMac",
+    "~/Library/Preferences/com.yuki.VCCMac.plist",
+    "~/Library/Saved Application State/com.yuki.VCCMac.savedState",
+  ]
+end
