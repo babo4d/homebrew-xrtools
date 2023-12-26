00d29e37d084e728f57dc318eb5417012fa8716f
babo <babomancer@gmail.com>
2023-12-25 16:58:03 -0800

    vrc-get 1.4.2

--- Formula/vrc-get.rb
+++ Formula/vrc-get.rb
@@ -2,14 +2,14 @@ class VrcGet < Formula
   desc "Command-line VPM Client"
   homepage "https://github.com/anatawa12/vrc-get"
   url "https://github.com/anatawa12/vrc-get.git",
-      tag:      "v1.4.0",
-      revision: "62bb941c0aab0c0aff39a28e07a0f7a3c32f735a"
+      tag:      "v1.4.2",
+      revision: "adf81e4f23ed87dbc563cd4ebb4c874a805d14bb"
   license "MIT"
 
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", *std_cargo_args
+    system "cargo", "install", *std_cargo_args(path: "vrc-get")
   end
 
   test do
