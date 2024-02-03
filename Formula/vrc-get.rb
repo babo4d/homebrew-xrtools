9877e1d0ceb68b3fbf526575842140ae7ef58fa9
babo <babomancer@gmail.com>
2024-02-03 12:17:52 -0800

    vrc-get 1.5.3

--- Formula/vrc-get.rb
+++ Formula/vrc-get.rb
@@ -2,8 +2,8 @@ class VrcGet < Formula
   desc "Command-line VPM Client"
   homepage "https://github.com/anatawa12/vrc-get"
   url "https://github.com/anatawa12/vrc-get.git",
-      tag:      "v1.4.2",
-      revision: "adf81e4f23ed87dbc563cd4ebb4c874a805d14bb"
+      tag:      "v1.5.3",
+      revision: "3e1e2ac6274232882de2a07bbf4f27e9d9b8c0ab"
   license "MIT"
 
   depends_on "rust" => :build
