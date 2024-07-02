a2c112bbf45939537dd47680268883ec36d71feb
babo <babomancer@gmail.com>
2024-07-01 23:09:13 -0700

    citra-pablomk7 20240615

---
+++ Casks/citra-pablomk7.rb
@@ -0,0 +1,30 @@
+cask "citra-pablomk7" do
+  version "20240615,4f174f1"
+  sha256 "921bacd1ae6caf3128a9eb0caeb60c444e20c097feb562dd09a907e65485bcb2"
+
+  url "https://github.com/PabloMK7/citra/releases/download/r#{version.csv.second}/citra-macos-universal-#{version.csv.first}-#{version.csv.second}.7z",
+      verified: "github.com/PabloMK7/citra/"
+  name "Citra"
+  desc "Nintendo 3DS emulator (PabloMK7 fork)"
+  homepage "https://citra-emulator.com/"
+
+  livecheck do
+    url :url
+    regex(/citra-macos-universal[._-]v?(\d+)[._-](\w+)\.7z/i)
+    strategy :github_latest do |json, regex|
+      json["assets"]&.map do |asset|
+        match = asset["name"]&.match(regex)
+        next if match.blank?
+
+        "#{match[1]},#{match[2]}"
+      end
+    end
+  end
+
+  app "head/citra-qt.app"
+
+  zap trash: [
+    "~/Library/Application Support/Citra",
+    "~/Library/Saved Application State/com.citra-emu.citra.savedState",
+  ]
+end
