2734326686c018c7fa32c06c4b0bb5dacaefae3b
babo <babomancer@gmail.com>
2024-07-01 23:10:23 -0700

    ryujinx 1.1.1340

---
+++ Casks/ryujinx.rb
@@ -0,0 +1,18 @@
+cask "ryujinx" do
+  version "1.1.1340"
+  sha256 "a0c6c7b906121508d922fb44dd4bb74bfb3c9cb0d68b9dd69f384e7c86ba0cde"
+
+  url "https://github.com/Ryujinx/release-channel-master/releases/download/#{version}/ryujinx-#{version}-macos_universal.app.tar.gz",
+      verified: "github.com/Ryujinx/release-channel-master/"
+  name "Ryujinx"
+  desc "Experimental Nintendo Switch Emulator written in C#"
+  homepage "https://www.ryujinx.org/"
+
+  app "Ryujinx.app"
+
+  zap trash: [
+    "~/Library/Application Support/Ryujinx",
+    "~/Library/Logs/Ryujinx",
+    "~/Library/Preferences/org.ryujinx.Ryujinx.plist",
+  ]
+end
