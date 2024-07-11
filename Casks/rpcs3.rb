894a1a8f839258842a98d0aecee8d8abc7d07df0
babo <babomancer@gmail.com>
2024-07-10 20:52:58 -0700

    rpcs3 0.0.32-16682

---
+++ Casks/rpcs3.rb
@@ -0,0 +1,33 @@
+cask "rpcs3" do
+  version "0.0.32-16682,99b44cf33a78698df57355534b51b9eb3202660f"
+  sha256 "eb5350fa45b8989e0e6a27df9e29536cf54db5b082a37cbc310dd0f2378efd0a"
+
+  url "https://github.com/RPCS3/rpcs3-binaries-mac/releases/download/build-#{version.csv.second}/rpcs3-v#{version.csv.first}-#{version.csv.second[0..7]}_macos.7z",
+      verified: "github.com/RPCS3/rpcs3-binaries-mac/"
+  name "RPCS3"
+  desc "PS3 emulator"
+  homepage "https://rpcs3.net/"
+
+  livecheck do
+    url "https://api.github.com/repos/RPCS3/rpcs3-binaries-mac/releases/latest"
+    regex(%r{.*/releases/download/build-(.+)/rpcs3-v(\d+\.\d+\.\d+-\d+)-(.+)_macos.7z}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map do |match|
+        "#{match[1]},#{match[0]}"
+      end
+    end
+  end
+
+  app "RPCS3.app"
+
+  zap trash: [
+    "~/Library/Application Support/rpcs3",
+    "~/Library/Caches/rpcs3",
+    "~/Library/Preferences/net.rpcs3.rpcs3.plist",
+    "~/Library/Saved Application State/net.rpcs3.rpcs3.savedState",
+  ]
+
+  caveats do
+    requires_rosetta
+  end
+end
