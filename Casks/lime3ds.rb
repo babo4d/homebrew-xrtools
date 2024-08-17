5c1842e926bfee6470fe92c7d94db11d5c3ee29a
babo <babomancer@gmail.com>
2024-08-17 15:42:54 -0700

    lime3ds 2117.1

--- Casks/lime3ds.rb
+++ Casks/lime3ds.rb
@@ -1,6 +1,6 @@
 cask "lime3ds" do
-  version "2116"
-  sha256 "a406ae3df6ca7408d7f56597969af68d6cd31f3d41dfc49e887c4ce79030e137"
+  version "2117.1"
+  sha256 "56ff33caf6b4680c0e11cc170d283d352848a41d345ff5ff52c0beaea5705095"
 
   url "https://github.com/Lime3DS/Lime3DS/releases/download/#{version}/lime3ds-#{version}-macos-universal.tar.gz",
       verified: "github.com/Lime3DS/Lime3DS/"
