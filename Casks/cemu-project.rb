a1a8f5630f375fe05f4b64dcad66f5798567cdeb
babo <babomancer@gmail.com>
2024-08-27 21:08:06 -0700

    cemu-project 2.1

--- Casks/cemu-project.rb
+++ Casks/cemu-project.rb
@@ -1,6 +1,6 @@
 cask "cemu-project" do
-  version "2.0-94"
-  sha256 "9f69aa891c608d2f59d41b24754fec97bbc426295da2a0070008d0947faaac53"
+  version "2.1"
+  sha256 "4e53959f286c7e1f8b618e21b3974b1bf72d3a6ad4ec1dd93cf94cff3206316d"
 
   url "https://github.com/cemu-project/Cemu/releases/download/v#{version}/cemu-#{version}-macos-12-x64.dmg",
       verified: "github.com/cemu-project/Cemu/"
