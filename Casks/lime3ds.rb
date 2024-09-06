d7bcf8fea39e85aa2a3b12745fb9fa1ad113f2e9
babo <babomancer@gmail.com>
2024-09-05 22:24:24 -0700

    lime3ds 2118

--- Casks/lime3ds.rb
+++ Casks/lime3ds.rb
@@ -1,17 +1,18 @@
 cask "lime3ds" do
-  version "2117.1"
-  sha256 "56ff33caf6b4680c0e11cc170d283d352848a41d345ff5ff52c0beaea5705095"
+  version "2118"
+  sha256 "12e141908425d21a9d78f11f5ff03ca7426a55bfbfd038c0a8fba51671646989"
 
-  url "https://github.com/Lime3DS/Lime3DS/releases/download/#{version}/lime3ds-#{version}-macos-universal.tar.gz",
+  url "https://github.com/Lime3DS/Lime3DS/releases/download/#{version}/lime3ds-#{version}-macos-universal.zip",
       verified: "github.com/Lime3DS/Lime3DS/"
   name "Lime3DS"
   desc "Nintendo 3DS emulator"
   homepage "https://lime3ds.github.io/"
 
-  app "lime3ds-#{version}-macos-universal/lime3ds-gui.app"
+  app "lime3ds-#{version}-macos-universal/lime3ds.app"
 
   zap trash: [
-    "~/Library/Application Support/Citra",
+    "~/Library/Application Support/Lime3DS",
+    "~/Library/Preferences/io.github.lime3ds.Lime3DS.plist",
     "~/Library/Saved Application State/io.github.lime3ds.Lime3DS.savedState",
   ]
 end
