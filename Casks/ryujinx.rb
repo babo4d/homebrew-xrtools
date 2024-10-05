f48d42d7f69ce6c4f2fc92ffc9026afa8946cf52
babo <babomancer@gmail.com>
2024-10-05 15:06:01 -0700

    ryujinx 1.1.1403 (last official version)

--- Casks/ryujinx.rb
+++ Casks/ryujinx.rb
@@ -1,12 +1,16 @@
 cask "ryujinx" do
-  version "1.1.1386"
-  sha256 "270e51d5fb5106eb606673eba41b81ddc4c4b0dac11c2ac2fe53ed4872c93f8f"
+  version "1.1.1403"
+  sha256 "07083574edf1a4a19d0ef4c82ef1baa07cca8465395350a750073f083d0c4053"
 
-  url "https://github.com/Ryujinx/release-channel-master/releases/download/#{version}/ryujinx-#{version}-macos_universal.app.tar.gz",
-      verified: "github.com/Ryujinx/release-channel-master/"
+  url "https://archive.org/download/ryujinx-1.1.1403_202410/ryujinx-#{version}-macos_universal.app.tar.gz",
+      verified: "archive.org/download/ryujinx-1.1.1403_202410/"
   name "Ryujinx"
   desc "Experimental Nintendo Switch Emulator written in C#"
   homepage "https://www.ryujinx.org/"
+
+  livecheck do
+    skip "Legacy version"
+  end
 
   app "Ryujinx.app"
 
