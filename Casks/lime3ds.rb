78fdc149db587b7aac5643cd30bfcddfa3ba722e
babo <babomancer@gmail.com>
2025-01-14 20:31:07 -0800

    lime3ds 2119.1

--- Casks/lime3ds.rb
+++ Casks/lime3ds.rb
@@ -1,12 +1,14 @@
 cask "lime3ds" do
-  version "2118.2"
-  sha256 "8c95759e5f1daad5e957148d1d921c36e01d87b18a3cd1e011a4ff48c6ea1fb4"
+  version "2119.1"
+  sha256 "9a6a10c543fdeda1b6c3c2c13c33e956870edca1c75153e5c1665e02ff6b5e33"
 
-  url "https://github.com/Lime3DS/Lime3DS/releases/download/#{version}/lime3ds-#{version}-macos-universal.zip",
-      verified: "github.com/Lime3DS/Lime3DS/"
+  url "https://github.com/Lime3DS/lime3ds-archive/releases/download/#{version}/lime3ds-#{version}-macos-universal.zip",
+      verified: "github.com/Lime3DS/lime3ds-archive/"
   name "Lime3DS"
   desc "Nintendo 3DS emulator"
   homepage "https://lime3ds.github.io/"
+
+  deprecate! date: "2024-11-16", because: :discontinued
 
   app "lime3ds-#{version}-macos-universal/lime3ds.app"
 
