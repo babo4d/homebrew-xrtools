41853b1ad9ab8cf3c0651140509c162001428983
babo <babomancer@gmail.com>
2025-01-24 15:08:27 -0800

    ryujinx 1.2.81

--- Casks/ryujinx.rb
+++ Casks/ryujinx.rb
@@ -1,8 +1,8 @@
 cask "ryujinx" do
-  version "1.2.78"
-  sha256 "7ede71b85925a8db38a27767d997d2d62a0a057b43d05e172b951ae439d8bb47"
+  version "1.2.81"
+  sha256 "ad444ebe3cd03ee1cda30723f7f8d16e4319f6c2374e9d2f9ad8f19adf04a724"
 
-  url "https://github.com/Ryubing/Ryujinx/releases/download/1.2.78/ryujinx-#{version}-macos_universal.app.tar.gz"
+  url "https://github.com/Ryubing/Ryujinx/releases/download/#{version}/ryujinx-#{version}-macos_universal.app.tar.gz"
   name "Ryujinx"
   desc "Nintendo Switch Emulator written in C#"
   homepage "https://github.com/Ryubing/Ryujinx"
