1a6bf6a9cf1fc1b6d03c74edfb06e2ce4f8391da
babo <babomancer@gmail.com>
2025-03-05 20:23:41 -0800

    rpcs3 0.0.35-17571

--- Casks/rpcs3.rb
+++ Casks/rpcs3.rb
@@ -1,9 +1,13 @@
 cask "rpcs3" do
-  version "0.0.34-17383,f1f85335a73d7134ad0ba783c32ab75debbea15a"
-  sha256 "aeea3f5eadfbc6dff0e984d4c5e8da1218db5598b1269b2800eb8b4c1479df7e"
+  file_suffix = on_arch_conditional arm: "macos_arm64", intel: "macos"
+  folder_suffix = on_arch_conditional arm: "mac-arm64", intel: "mac"
 
-  url "https://github.com/RPCS3/rpcs3-binaries-mac/releases/download/build-#{version.csv.second}/rpcs3-v#{version.csv.first}-#{version.csv.second[0..7]}_macos.7z",
-      verified: "github.com/RPCS3/rpcs3-binaries-mac/"
+  version "0.0.35-17571,583ec5d819c4a28d22e1819252d8d8d3d0c02db4"
+  sha256 arm:   "07444dc13a13f240e112dc994b46b83fb842b57344e71da3a7c95b0f7c40e45b",
+         intel: "ee8e15351ee52e8cb8985987340b64737ffb84ee645316357492b46d5d138ebb"
+
+  url "https://github.com/RPCS3/rpcs3-binaries-#{folder_suffix}/releases/download/build-#{version.csv.second}/rpcs3-v#{version.csv.first}-#{version.csv.second[0..7]}_#{file_suffix}.7z",
+      verified: "github.com/RPCS3/rpcs3-binaries-#{folder_suffix}/"
   name "RPCS3"
   desc "PS3 emulator"
   homepage "https://rpcs3.net/"
@@ -26,8 +30,4 @@ cask "rpcs3" do
     "~/Library/Preferences/net.rpcs3.rpcs3.plist",
     "~/Library/Saved Application State/net.rpcs3.rpcs3.savedState",
   ]
-
-  caveats do
-    requires_rosetta
-  end
 end
