afb0459e0f76136406f2e4c64ccba3b6e25f6a66
babo <babomancer@gmail.com>
2025-03-05 20:16:33 -0800

    openra-combined-arms 1.05.1

--- Casks/openra-combined-arms.rb
+++ Casks/openra-combined-arms.rb
@@ -1,6 +1,6 @@
 cask "openra-combined-arms" do
-  version "1.04"
-  sha256 "dd02c5ed8b2bc4777cfa992289938d6c1b1a19b6efd91acb71a3bd62df4397fe"
+  version "1.05.1"
+  sha256 "f263d6d5d6738cc0db9298f03ad8e92c423b71493aef34cda924e2b9c93bccba"
 
   url "https://github.com/Inq8/CAmod/releases/download/#{version}/CombinedArms-#{version}.dmg",
       verified: "github.com/Inq8/CAmod/"
