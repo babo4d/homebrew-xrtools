00c05721eda9f8d58367940db10516a2c7cdee31
babo <babomancer@gmail.com>
2025-03-22 17:37:01 -0700

    openra-sole-survivor 20250308,release

--- Casks/openra-sole-survivor.rb
+++ Casks/openra-sole-survivor.rb
@@ -1,6 +1,6 @@
 cask "openra-sole-survivor" do
-  version "20231105,release"
-  sha256 "a6a2a2243f64cbc6a5ebec03819fa73cf2b145294a6b8141281e573a32b0ae9c"
+  version "20250308,release"
+  sha256 "a00871bbad9162a9bd3295c60b91810f2af8a768f5d423b0114812a10be9bb82"
 
   url "https://github.com/MustaphaTR/sole-survivor/releases/download/#{version.csv.second}-#{version.csv.first}/SoleSurvivor-#{version.csv.second}-#{version.csv.first}.dmg"
   name "OpenRA - Sole Survivor"
