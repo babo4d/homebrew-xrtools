caa1b65ccb8731aaff29d09d4ab84fe108732e01
babo <babomancer@gmail.com>
2025-08-18 20:48:04 -0700

    vrcx 2025.08.17 (new cask)

---
+++ Casks/vrcx.rb
@@ -0,0 +1,29 @@
+cask "vrcx" do
+  version "2025.08.17"
+  sha256 "4745fd128ebc6a10b965c7fbb2388848b8791eabd941085a027723a02b398027"
+
+  url "https://github.com/vrcx-team/VRCX/releases/download/v#{version}/VRCX_#{version.no_dots}.dmg"
+  name "VRCX"
+  desc "Friendship management tool for VRChat"
+  homepage "https://github.com/vrcx-team/VRCX"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "VRCX.app"
+
+  zap trash: [
+    "~/Library/Application Support/VRCX",
+    "~/Library/Preferences/app.vrcx.plist",
+  ]
+
+  caveats do
+    requires_rosetta
+    <<~EOS
+      Requires dotnet runtime for x64 (Intel)
+        https://dotnet.microsoft.com/en-us/download
+    EOS
+  end
+end
