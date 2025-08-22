d8303bf4476540874360e3be63e27a50bfb1cc0b
babo <babomancer@gmail.com>
2025-08-22 16:41:40 -0700

    azahar 2123.1

--- Casks/azahar.rb
+++ Casks/azahar.rb
@@ -1,6 +1,6 @@
 cask "azahar" do
-  version "2122.1"
-  sha256 "a9adf930747b65b962003fc0057c2dc95ebe0872a51e29e451785e9a9f34101b"
+  version "2123.1"
+  sha256 "2c4913684771d91526011c32260b7dbc478fb06d3f47635cbae646539da38828"
 
   url "https://github.com/azahar-emu/azahar/releases/download/#{version}/azahar-#{version}-macos-universal.zip",
       verified: "github.com/azahar-emu/azahar/"
