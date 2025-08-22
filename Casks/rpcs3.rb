cbce89d8163546b6d2dbf5ff896f71b6c7f0bad1
babo <babomancer@gmail.com>
2025-08-22 16:52:55 -0700

    rpcs3 0.0.37-18107

--- Casks/rpcs3.rb
+++ Casks/rpcs3.rb
@@ -2,9 +2,9 @@ cask "rpcs3" do
   file_suffix = on_arch_conditional arm: "macos_arm64", intel: "macos"
   folder_suffix = on_arch_conditional arm: "mac-arm64", intel: "mac"
 
-  version "0.0.37-18014,27359d3d329693c3f6bab2d480a08603511f2261"
-  sha256 arm:   "f47104e4b45299db4bd08c9ca3ccc9e6e9989c770ce9cbf2aee72d034590bd0d",
-         intel: "b4adf851032b5c532012b23eee176a96ae8d411863ea2f124a5cbae86c3cf869"
+  version "0.0.37-18107,246519c5e15ecc7e60d72cee909a1b5e12fb3cb7"
+  sha256 arm:   "21d68f9ed2bf02a0efa808251292f4e9b7f2b0c908ce083503f8af9004b882e9",
+         intel: "f73b44fe6039bc2e7f297d8290c3d1a911e8072c0c760dfa68327fcc1e96cd41"
 
   url "https://github.com/RPCS3/rpcs3-binaries-#{folder_suffix}/releases/download/build-#{version.csv.second}/rpcs3-v#{version.csv.first}-#{version.csv.second[0..7]}_#{file_suffix}.7z",
       verified: "github.com/RPCS3/rpcs3-binaries-#{folder_suffix}/"
