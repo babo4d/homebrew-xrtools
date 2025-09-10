8d98912b375e3e9e1d63c2e155038ef38b8b65f0
babo <babomancer@gmail.com>
2025-09-09 21:21:08 -0700

    vrcx 2025.09.10

--- Casks/vrcx.rb
+++ Casks/vrcx.rb
@@ -1,8 +1,11 @@
 cask "vrcx" do
-  version "2025.08.17"
-  sha256 "4745fd128ebc6a10b965c7fbb2388848b8791eabd941085a027723a02b398027"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://github.com/vrcx-team/VRCX/releases/download/v#{version}/VRCX_#{version.no_dots}.dmg"
+  version "2025.09.10"
+  sha256 arm:   "abe45069cdfff0e2cca54ea88fd23bef2f8e910ea7c14c7a82867967ca7d0851",
+         intel: "7fe7dee5c73f60feddb964b0edb456eebff0848485be1192c2244088cbd11e96"
+
+  url "https://github.com/vrcx-team/VRCX/releases/download/v#{version}/VRCX_#{version.no_dots}_#{arch}.dmg"
   name "VRCX"
   desc "Friendship management tool for VRChat"
   homepage "https://github.com/vrcx-team/VRCX"
@@ -12,20 +15,10 @@ cask "vrcx" do
     strategy :github_latest
   end
 
-  depends_on cask: "dotnet-runtime-x64"
-
   app "VRCX.app"
 
   zap trash: [
     "~/Library/Application Support/VRCX",
     "~/Library/Preferences/app.vrcx.plist",
   ]
-
-  caveats do
-    requires_rosetta
-    <<~EOS
-      Requires dotnet for x64 (Intel)
-        https://dotnet.microsoft.com/en-us/download
-    EOS
-  end
 end
