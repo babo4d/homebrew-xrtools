fda360536d906783c0f46fef38d7b4be7eee8787
babo <babomancer@gmail.com>
2025-10-11 08:12:10 -0700

    shadps4 0.11.0

--- Casks/shadps4.rb
+++ Casks/shadps4.rb
@@ -1,6 +1,6 @@
 cask "shadps4" do
-  version "0.10.0"
-  sha256 "844a1504bc8157343650af14b2fcc488a8ce2aacfcf0bbd664cd462fd3daba4b"
+  version "0.11.0"
+  sha256 "00b56c74b818f4597468c42d3ff0700d772ca095c0e7668a6e2ca6fe900af44d"
 
   url "https://github.com/shadps4-emu/shadPS4/releases/download/v.#{version}/shadps4-macos-qt-#{version}.zip",
       verified: "github.com/shadps4-emu/"
@@ -12,6 +12,8 @@ cask "shadps4" do
     url :url
     strategy :github_latest
   end
+
+  depends_on macos: ">= :sequoia"
 
   app "shadps4.app"
 
