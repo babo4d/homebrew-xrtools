fcbdeaa2559e910658d9038cd78de00f2652f4e4
babo <babomancer@gmail.com>
2025-10-11 08:10:26 -0700

    godots 1.4.stable

--- Casks/godots.rb
+++ Casks/godots.rb
@@ -1,6 +1,6 @@
 cask "godots" do
-  version "1.3.stable"
-  sha256 "790f83b2a12815190848129d3b58c1ba52b8d5eec0809a943e3ee01c503db92c"
+  version "1.4.stable"
+  sha256 "1e1282ad0bce1796196ab60100d0c1ab78e02b9aaa64a10bb3263df67860390f"
 
   url "https://github.com/MakovWait/godots/releases/download/v#{version}/macOS.zip"
   name "Godots"
