e68949a58c533c7f88f1237a521236c3ac330366
babo <babomancer@gmail.com>
2025-10-11 08:09:10 -0700

    azahar 2123.3

--- Casks/azahar.rb
+++ Casks/azahar.rb
@@ -1,12 +1,14 @@
 cask "azahar" do
-  version "2123.1"
-  sha256 "2c4913684771d91526011c32260b7dbc478fb06d3f47635cbae646539da38828"
+  version "2123.3"
+  sha256 "5a43ac81713544de0e463f430433f7b2bd20c6efda6c2705f8dbc43f7936afaa"
 
   url "https://github.com/azahar-emu/azahar/releases/download/#{version}/azahar-#{version}-macos-universal.zip",
       verified: "github.com/azahar-emu/azahar/"
   name "Azahar"
   desc "Nintendo 3DS emulator"
   homepage "https://azahar-emu.org/"
+
+  depends_on macos: ">= :ventura"
 
   app "azahar-#{version}-macos-universal/Azahar.app"
 
