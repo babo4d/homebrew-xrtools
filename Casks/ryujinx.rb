aa6c959849a836896d472802e21f883bf99abb14
babo <babomancer@gmail.com>
2025-10-11 08:12:46 -0700

    ryujinx 1.3.3

--- Casks/ryujinx.rb
+++ Casks/ryujinx.rb
@@ -1,6 +1,6 @@
 cask "ryujinx" do
-  version "1.3.2"
-  sha256 "c157907318348999ce4fb8c8fe8ddd511f0ddea643f22b1529527390180cb700"
+  version "1.3.3"
+  sha256 "e4818bb84c98e0d3120691821e90772099e46101273d3f145ffdb10eee2c0dbb"
 
   url "https://git.ryujinx.app/api/v4/projects/1/packages/generic/Ryubing/#{version}/ryujinx-#{version}-macos_universal.app.tar.gz"
   name "Ryujinx"
@@ -11,6 +11,8 @@ cask "ryujinx" do
     url "https://git.ryujinx.app/ryubing/ryujinx/-/releases/permalink/latest"
     strategy :header_match
   end
+
+  depends_on macos: ">= :monterey"
 
   app "Ryujinx.app"
 
