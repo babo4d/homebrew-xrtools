c49949808728a8d1b6a2d5fca880a9d483c73a0f
babo <babomancer@gmail.com>
2025-11-13 20:10:38 -0800

    blender-launcher 2.5.3

--- Casks/blender-launcher.rb
+++ Casks/blender-launcher.rb
@@ -1,6 +1,6 @@
 cask "blender-launcher" do
-  version "2.5.2"
-  sha256 "c918024fe83d3b15741a9f4ed312312462b2d21430071dc85417190c0484c171"
+  version "2.5.3"
+  sha256 "06162edc6f9c03feb3ff5e692520d8c086a6805562ec60e1412838b1461cf189"
 
   url "https://github.com/Victor-IX/Blender-Launcher-V2/releases/download/v#{version}/Blender_Launcher_v#{version}_macos_arm64.zip"
   name "Blender Launcher"
