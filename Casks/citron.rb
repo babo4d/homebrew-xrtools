6b26ec57b73b6a9fdedb54585c0bba992b6ea010
babo <babomancer@gmail.com>
2025-12-26 17:07:19 -0800

    citron 0.12.25

--- Casks/citron.rb
+++ Casks/citron.rb
@@ -1,8 +1,9 @@
 cask "citron" do
-  version "0.11.0"
-  sha256 "224d5503c20b9fb23401860ae01ff817dd246afcf12d413b8b4f2cc79ea82a86"
+  version "0.12.25"
+  sha256 "a0fdddf100da4d502aac29d12ca696644abd54f7f8c8ec26e97c5c211a808b07"
 
-  url "https://git.citron-emu.org/Citron/Emulator/releases/download/#{version}/Citron-macOS-stable-#{version}.dmg"
+  # hard coded hash for version 0.12.25
+  url "https://git.citron-emu.org/Citron/Emulator/releases/download/#{version}/Citron-macOS-stable-01c042048.dmg"
   name "Citron"
   desc "Advanced Nintendo Switch Emulator"
   homepage "https://citron-emu.org/"
