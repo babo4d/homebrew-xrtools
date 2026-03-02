d4b5909da92f4b9a7c51bb46bdad81db52fe000a
babo <babomancer@gmail.com>
2026-03-01 16:31:35 -0800

    vrcx 2026.02.11

--- Casks/vrcx.rb
+++ Casks/vrcx.rb
@@ -1,9 +1,9 @@
 cask "vrcx" do
   arch arm: "arm64", intel: "x64"
 
-  version "2026.01.04"
-  sha256 arm:   "e27a0a9a564ff28b71357feec19c8868b6af839963c54c3c32c5bff5134fd8d9",
-         intel: "21ce2615efa6e76680ff62e8fcfe0c36917b957b958264044d43cc1d5f6ca448"
+  version "2026.02.11"
+  sha256 arm:   "92adbd5141678b579da335c01becafa7f86feeaddb3539aa3193351a33c2c694",
+         intel: "ed5da8f3259a5b941c11eef947d01c1b412b61a42b01b1f29e2760f2a060e869"
 
   url "https://github.com/vrcx-team/VRCX/releases/download/v#{version}/VRCX_#{version}_#{arch}.dmg"
   name "VRCX"
