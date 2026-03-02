f680d68873c477b244098b53524bd08043349429
babo <babomancer@gmail.com>
2026-03-01 16:48:36 -0800

    eden 0.2.0-rc1

--- Casks/eden.rb
+++ Casks/eden.rb
@@ -1,6 +1,6 @@
 cask "eden" do
-  version "0.0.4"
-  sha256 "fd094f0d50f3fd08c5a0709aba8a2aeab7309b11c052427a4823fd1ff4c00a5f"
+  version "0.2.0-rc1"
+  sha256 "c1fec63d61d9346eb69e3bc63fcd1610b879df1f9ef492e83cd9e27aa081f654"
 
   url "https://github.com/eden-emulator/Releases/releases/download/v#{version}/Eden-macOS-v#{version}.tar.gz",
       verified: "github.com/eden-emulator/"
