8483b54d1fa3ca87c330a41dd8d2de7d14e532dc
babo <babomancer@gmail.com>
2026-05-17 19:10:19 -0700

    ryujinx: fix links

--- Casks/ryujinx.rb
+++ Casks/ryujinx.rb
@@ -2,17 +2,17 @@ cask "ryujinx" do
   version "1.3.3"
   sha256 "e4818bb84c98e0d3120691821e90772099e46101273d3f145ffdb10eee2c0dbb"
 
-  url "https://git.ryujinx.app/api/v4/projects/1/packages/generic/Ryubing/#{version}/ryujinx-#{version}-macos_universal.app.tar.gz"
+  url "https://git.ryujinx.app/projects/Ryubing/releases/download/#{version}/ryujinx-#{version}-macos_universal.app.tar.gz"
   name "Ryujinx"
   desc "Nintendo Switch Emulator written in C#"
   homepage "https://ryujinx.app/"
 
   livecheck do
-    url "https://git.ryujinx.app/ryubing/ryujinx/-/releases/permalink/latest"
+    url "https://git.ryujinx.app/projects/Ryubing/releases/latest"
     strategy :header_match
   end
 
-  depends_on macos: ">= :monterey"
+  depends_on macos: :monterey
 
   app "Ryujinx.app"
 
