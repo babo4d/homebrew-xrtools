84bfe5af2f3910a12d7a6c798500693a7a95555a
babo <babomancer@gmail.com>
2026-05-17 18:49:34 -0700

    steam-rom-manager 2.5.34

--- Casks/steam-rom-manager.rb
+++ Casks/steam-rom-manager.rb
@@ -1,12 +1,14 @@
 cask "steam-rom-manager" do
-  version "2.5.33"
-  sha256 "a8d4c7f7a763356bee1fe21be937c6a3a0a5527da5c4927ebf62cfb090f1b777"
+  version "2.5.34"
+  sha256 "fd929377ce9d9324375e9207293f963e198a2d2f4b46851658c963ca10b57911"
 
   url "https://github.com/SteamGridDB/steam-rom-manager/releases/download/v#{version}/Steam-ROM-Manager-#{version}.dmg",
       verified: "github.com/SteamGridDB/steam-rom-manager/"
   name "Steam ROM Manager"
   desc "Tool for adding non-Steam games to Steam in bulk"
   homepage "https://steamgriddb.github.io/steam-rom-manager/"
+
+  depends_on :macos
 
   app "Steam ROM Manager.app"
 
