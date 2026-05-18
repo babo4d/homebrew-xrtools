413cee6d9debd9bf0aa9857c6b30bf43ec11ebe3
babo <babomancer@gmail.com>
2026-05-17 18:48:46 -0700

    vrcx 2026.05.03

--- Casks/vrcx.rb
+++ Casks/vrcx.rb
@@ -1,9 +1,9 @@
 cask "vrcx" do
   arch arm: "arm64", intel: "x64"
 
-  version "2026.02.11"
-  sha256 arm:   "92adbd5141678b579da335c01becafa7f86feeaddb3539aa3193351a33c2c694",
-         intel: "ed5da8f3259a5b941c11eef947d01c1b412b61a42b01b1f29e2760f2a060e869"
+  version "2026.05.03"
+  sha256 arm:   "195da71d1f333bee66a1e4600e65cb4421c36d0d6a1bb8ab889ef61bf30e86dc",
+         intel: "7523ce1b5339c761929599ec2a9771fcc89e6ea6a6ad577a1d9f81a9d99cd7d6"
 
   url "https://github.com/vrcx-team/VRCX/releases/download/v#{version}/VRCX_#{version}_#{arch}.dmg"
   name "VRCX"
@@ -15,7 +15,7 @@ cask "vrcx" do
     strategy :github_latest
   end
 
-  depends_on macos: ">= :monterey"
+  depends_on macos: :monterey
 
   app "VRCX.app"
 
