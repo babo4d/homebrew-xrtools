fbe4e48d61edf76096638740e186d79d480e4d0d
babo <babomancer@gmail.com>
2026-05-17 18:50:12 -0700

    eden 0.2.0

--- Casks/eden.rb
+++ Casks/eden.rb
@@ -1,8 +1,8 @@
 cask "eden" do
-  version "0.2.0-rc2"
-  sha256 "6987944af48380667c716a55b237620872d8402c98acd8f84346d278d87331cb"
+  version "0.2.0"
+  sha256 "745811617b7983f0f538e46c71d3637c85c64bccbe640bf6f11af9afd550da22"
 
-  url "https://git.eden-emu.dev/eden-emu/eden/releases/download/v#{version}/Eden-macOS-v#{version}.tar.gz"
+  url "https://git.eden-emu.dev/eden-emu/eden/releases/download/v#{version}/Eden-macOS-v#{version}.dmg"
   name "Eden Emulator"
   desc "Nintendo Switch emulator"
   homepage "https://eden-emu.dev/"
@@ -12,6 +12,8 @@ cask "eden" do
     regex(/v?(\d+(?:\.\d+)+[-\w]*)/i)
     strategy :git
   end
+
+  depends_on :macos
 
   app "eden.app"
 
