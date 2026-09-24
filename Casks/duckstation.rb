ba796d3ee80ede4ab77f9ae03c98540841d3a504
babo <babomancer@gmail.com>
2026-09-23 22:11:49 -0700

    duckstation 0.1-11826

--- Casks/duckstation.rb
+++ Casks/duckstation.rb
@@ -1,6 +1,6 @@
 cask "duckstation" do
-  version "0.1-11752"
-  sha256 "fedf0a6863210f703b4c71b697e78a17a306121b73f7cd562346c061279351e3"
+  version "0.1-11826"
+  sha256 "1870ef7c34f619861cf9bedfe73772cb2106061058ce6e40a7b41dc328835a96"
 
   url "https://github.com/stenzek/duckstation/releases/download/v#{version}/duckstation-mac-release.zip"
   name "DuckStation"
